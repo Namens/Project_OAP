4da81fc821d4129f3bb645d481519a89a6000713
Antwar <antwargame@mail.ru>
2024-02-10 22:05:39 +0500

    AllFunctions

--- app.js
+++ app.js
@@ -1,79 +1,81 @@
 const apiKey = 'b45887a7acfd4a448a1125042240902'
+
 const form = document.querySelector('#form')
 const input = document.querySelector('#inputCity')
 const header = document.querySelector('.header')
 
 
-function showError (errorMessage) {
-
-}
-
-
 function removeCard () {
-
-}
-
-function showCard () {
-
+    const prevCard = document.querySelector('.card-box');
+	if (prevCard) prevCard.remove();
 }
 
 
-
-form.onsubmit = function (event) {
-    event.preventDefault()
-
-    let city = input.value.trim()
+function showError (errorMessage) {
+    const html = `<main class="main">
+    <section class="section">
+        <div class="card-box">${errorMessage}</div>
+    </section>
+    </main>`
     
-    const prevCard = document.querySelector('.card-box')
-
-    if (prevCard) prevCard.remove()
-
+    header.insertAdjacentHTML ('afterend', html)
+}
 
 
 
+function showCard (name, country, temp_c, condition) {
 
+    const html = `    <main class="main">
+    <section class="section">
+        <div class="card-box">
+            <h2 class="section-title-text section-title-text-h2">${name} <span>${country}</span></h2>
+              
+            <div class="card-weather">
+                <div class="section-text-main section-text-p">${temp_c}<sup>°с</sup></div>
+                <img src="./images/icons_svg/icon-weather.svg" alt="weather" class="icon-weather">
+            </div>
+                
+            <div class="section-text-footer section-text-footer-p">${condition}</div>
+        </div>
+    </section>
+</main>`
+
+    header.insertAdjacentHTML ('afterend', html)
+}
+
+async function getWeather(city) {
     const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`
+    const response = await fetch(url)
+    const data = await response.json()
+    console.log(data)   
+    return data
+}
 
 
-    fetch(url).then((response) => {
-        return response.json()
-    }).then((data) => {
+form.onsubmit = async function (event) {
+    event.preventDefault()
+    
+    let city = input.value.trim()
+    const data = await getWeather(city) // получаем данные с сервера
+    
 
-        if (data.error) {
+    if (data.error) {
 
-        if (prevCard) prevCard.remove()
-
-        const html = `<main class="main">
-        <section class="section">
-            <div class="card-box">${data.error.message}</div>
-        </section>
-        </main>`
-        
-        
-        header.insertAdjacentHTML ('afterend', html)
+        removeCard()
+        showError(data.error.message)
 
         } else {
 
-            const html = `    <main class="main">
-            <section class="section">
-                <div class="card-box">
-                    <h2 class="section-title-text section-title-text-h2">${data.location.name} <span>${data.location.country}</span></h2>
-                      
-                    <div class="card-weather">
-                        <div class="section-text-main section-text-p">${data.current.temp_c}<sup>°с</sup></div>
-                        <img src="./images/icons_svg/icon-weather.svg" alt="weather" class="icon-weather">
-                    </div>
-                        
-                    <div class="section-text-footer section-text-footer-p">${data.current.condition.text}}</div>
-                </div>
-            </section>
-    </main>`
-    
-            header.insertAdjacentHTML ('afterend', html)
+        removeCard()
 
+        showCard(
+            data.location.name,
+            data.location.country,
+            data.current.temp_c,
+            data.current.condition.text,
+        )
 
         }
 
 
-    })
 }
